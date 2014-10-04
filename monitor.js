0d5a205a619e193f13a6856bd6ceb9d003dc3034
Jonathan Budzenski <jon@jbudz.me>
2014-10-04 14:58:38 -0500

    naive interval checking

--- monitor.js
+++ monitor.js
@@ -1,2 +1,47 @@
 var status = require('./lib/status');
 var codes = require('./lib/codes');
+
+var statusInterval;
+var domains = [];
+
+module.exports = {
+	start: function(options, success, error) {
+		if(domains.length === 0) {
+			throw new Error("You need to add a domain before starting");
+		}
+
+		options = options || {
+			interval: 5
+		};
+
+		options.interval = Math.ceil(options.interval)*60000
+
+		statusInterval = setInterval(function() {
+			domains.forEach(function(domain) {
+				status.check(domain, function(res) {
+					if(res.state === codes.UP && success) {
+						success(res)
+					} else if(error) {
+						error(res);
+					}
+				});
+			});
+		}, options.interval);
+	},
+	stop: function() {
+		clearInterval(statusInterval);
+	},
+	add: function(newDomain) {
+		if(newDomain instanceof Array) {
+			domains = domains.concat(newDomain)
+		} else if(typeof newDomain === 'string') {
+			domains.push(newDomain);
+		}
+	},
+	remove: function(domain) {
+		var index = domains.indexOf(domain);
+		if(index >= 0) {
+			domains.splice(index, 1);
+		}
+	}
+}
